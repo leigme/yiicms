0738abc824fb5774e44bdaae64cfdd6fd3749bca
leig <leigme@gmail.com>
2017-08-08 18:52:28 +0800

    添加后台页面模板

---
+++ protected/views/admin/footer.php
@@ -0,0 +1,4 @@
+<!-- #include file="copyright" -->
+
+<?php $this->beginContent('//comm/copyright');?>
+<?php $this->endContent();?>
---
+++ protected/views/admin/layout.php
@@ -0,0 +1,13 @@
+<!-- #include file="header" -->
+
+<?php $this->beginContent('//admin/header');?>
+<?php $this->endContent();?>
+
+<!-- #include file="content" -->
+
+<?php echo $content;?>
+
+<!-- #include file="footer" -->
+
+<?php $this->beginContent('//admin/footer');?>
+<?php $this->endContent();?> 
