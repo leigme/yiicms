ba7db792fe9276d1e5a196a0211ac7ffad61dbf7
leig <leigme@gmail.com>
2017-08-08 17:10:38 +0800

    添加评论模型

---
+++ protected/models/YcComment.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * This is the model class for table "yc_comment".
+ *
+ * The followings are the available columns in table 'yc_comment':
+ * @property integer $Id
+ * @property integer $ContentId
+ * @property integer $ParentId
+ * @property string $Title
+ * @property string $Content
+ * @property integer $Author
+ * @property integer $OperateFlag
+ * @property integer $DeleteFlag
+ * @property string $CreateTime
+ * @property string $UpdateTime
+ * @property string $DeleteTime
+ *
+ * The followings are the available model relations:
+ * @property YcContent $content
+ * @property YcUser $author
+ */
+class YcComment extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return YcComment the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'yc_comment';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('Id', 'required'),
+			array('Id, ContentId, ParentId, Author, OperateFlag, DeleteFlag', 'numerical', 'integerOnly'=>true),
+			array('Title, Content, CreateTime, UpdateTime, DeleteTime', 'safe'),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('Id, ContentId, ParentId, Title, Content, Author, OperateFlag, DeleteFlag, CreateTime, UpdateTime, DeleteTime', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'content' => array(self::BELONGS_TO, 'YcContent', 'ContentId'),
+			'author' => array(self::BELONGS_TO, 'YcUser', 'Author'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'Id' => 'ID',
+			'ContentId' => 'Content',
+			'ParentId' => 'Parent',
+			'Title' => 'Title',
+			'Content' => 'Content',
+			'Author' => 'Author',
+			'OperateFlag' => 'Operate Flag',
+			'DeleteFlag' => 'Delete Flag',
+			'CreateTime' => 'Create Time',
+			'UpdateTime' => 'Update Time',
+			'DeleteTime' => 'Delete Time',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('Id',$this->Id);
+		$criteria->compare('ContentId',$this->ContentId);
+		$criteria->compare('ParentId',$this->ParentId);
+		$criteria->compare('Title',$this->Title,true);
+		$criteria->compare('Content',$this->Content,true);
+		$criteria->compare('Author',$this->Author);
+		$criteria->compare('OperateFlag',$this->OperateFlag);
+		$criteria->compare('DeleteFlag',$this->DeleteFlag);
+		$criteria->compare('CreateTime',$this->CreateTime,true);
+		$criteria->compare('UpdateTime',$this->UpdateTime,true);
+		$criteria->compare('DeleteTime',$this->DeleteTime,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
