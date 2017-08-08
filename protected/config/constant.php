474fe66ba210792125f0b085efef8334be85c5bb
leig <leigme@gmail.com>
2017-08-08 10:16:07 +0800

    更新常量文件

--- protected/config/constant.php
+++ protected/config/constant.php
@@ -1,3 +1,13 @@
 <?php
+/**
+ * 项目常量表
+ * 
+ * @author leig
+ */
+
 // bootstrap根路径
 define('YC_BOOTSTRAP_PATH', "/static/bootstrap/");
+// bootstrapCSS样式路径
+define('YC_BOOTSTRAP_CSS', "/static/bootstrap/css/");
+// bootstrapJS路径
+define('YC_BOOTSTRAP_JS', "/static/bootstrap/js/");
